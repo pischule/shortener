cc7105cfa58bd2099f8bc3cb077f8be163828a58
Maksim Pischulenok <pischule@outlook.com>
2023-02-02 01:05:41 +0300

    Replace js login href with ssr

--- src/main/java/com/pischule/services/SecurityService.java
+++ src/main/java/com/pischule/services/SecurityService.java
@@ -1,7 +1,9 @@
 package com.pischule.services;
 
+import io.quarkus.oidc.IdToken;
 import io.quarkus.oidc.UserInfo;
 import io.smallrye.common.constraint.Nullable;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -14,14 +16,16 @@ public class SecurityService {
     @Inject
     UserInfo userInfo;
 
-
     @Inject
+    @IdToken
+    JsonWebToken idToken;
+
     @Nullable
     public String getUserId() {
-        return Optional.ofNullable(userInfo)
-                .filter(it -> it.getJsonObject() != null)
-                .map(it -> it.getLong("id"))
-                .map(it -> "github-" + it)
-                .orElse(null);
+        return Optional.ofNullable(idToken.getSubject())
+                .or(() -> Optional.ofNullable(userInfo)
+                        .filter(it -> it.getJsonObject() != null)
+                        .map(it -> it.getLong("id"))
+                        .map(it -> "github-" + it)).orElse(null);
     }
 }
