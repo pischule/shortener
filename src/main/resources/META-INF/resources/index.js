030ec3984abeb730f9d4e8c96b42637d2dae4949
Maksim Pischulenok <pischule@outlook.com>
2023-02-06 21:08:15 +0300

    Add security headers

---
+++ src/main/resources/META-INF/resources/index.js
@@ -0,0 +1,17 @@
+let copyTimeoutID = null;
+
+const redirectInput = document.querySelector("#redirect-input");
+const copyRedirectButton = document.querySelector("#copy-redirect-button");
+
+function copyRedirectToClipboard() {
+    redirectInput.select();
+    document.execCommand("copy")
+    copyRedirectButton.textContent = 'Copied';
+
+    clearInterval(copyTimeoutID);
+    copyTimeoutID = setTimeout(() => {
+        redirectInput.textContent = 'Copy';
+    }, 1000);
+}
+
+copyRedirectButton?.addEventListener('click', copyRedirectToClipboard);
